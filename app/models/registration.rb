bb7069dafab579ef2726dc678b5caf401c4dcb3b
Nick Quaranto <nick@quaran.to>
2012-05-15 09:13:40 -0400

    allow nil/blank twitter handles

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,9 +1,10 @@
 class Registration < ActiveRecord::Base
   attr_accessible :email, :name, :topic, :twitter
 
-  validates_uniqueness_of :name, :email, :twitter
+  validates_uniqueness_of :name, :email
+  validates_uniqueness_of :twitter, allow_nil: true, allow_blank: true
   validates_presence_of :name, :email
-  validates_format_of :email, :with => %r/.+\@.+\..+/
+  validates_format_of :email, with: %r/.+\@.+\..+/
 
   before_create do
     self.key = SecureRandom.hex(32)
