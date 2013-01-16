cef7b1a08d36475abbfa3a7940cf019af01cc1e1
Nick Quaranto <nick@quaran.to>
2013-01-15 20:02:33 -0500

    Update mailer and gmaps link

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,6 +3,6 @@ class Notifier < ActionMailer::Base
 
   def confirmation(registration)
     @registration = registration
-    mail to: registration.email, subject: "Thanks for registering for BarCamp Buffalo 5!"
+    mail to: registration.email, subject: "Thanks for registering for BarCamp Buffalo 6!"
   end
 end
